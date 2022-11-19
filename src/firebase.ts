abe49462317393618227891462603e8dcfaf5623
Vladislav <vladkruglovfener@gmail.com>
2022-11-19 20:39:00 +0200

    add sing up method by facebook

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAXBnHbQKKSJFhJKfnB_7owPsDowFWVJHs",
     authDomain: "react-spotify-50ca1.firebaseapp.com",
@@ -18,7 +13,6 @@ const firebaseConfig = {
     databaseURL: 'https://react-spotify-50ca1-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
